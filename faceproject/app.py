349d9238e90df464a47e9bcd1cb59daea171d6d9
Hyeonseoooo <ohs000913@gmail.com>
2024-12-02 15:09:44 +0900

    Update app.py

--- faceproject/app.py
+++ faceproject/app.py
@@ -1,18 +1,11 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template  # render_template 임포트
 
 app = Flask(__name__)
-CORS(app)
 
-# 기본 경로('/') 추가
+# 홈 페이지 라우트
 @app.route('/')
 def home():
-    return "홈 페이지입니다. API는 '/compare'에서 확인할 수 있습니다."
+    return render_template('index.html')  # templates 폴더의 index.html 파일을 렌더링
 
-@app.route('/compare', methods=['POST'])
-def compare_faces():
-    # 얼굴 비교 로직
-    return jsonify({"similarity": 95})
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
